910c9ba4a935d2b58d3e6ff817e7fe1aca626678
shirleywan <shirleyti1949@163.com>
2018-07-05 18:25:53 +0800

    create 66

---
+++ src/chap4/GetMedian66.java
@@ -0,0 +1,21 @@
+/**
+ * Created by c84104080 on 2018/7/5.
+ */
+import java.util.*;
+class Solution66 {
+
+    public void Insert(Integer num) {
+        
+    }
+
+    public Double GetMedian() {
+        return null;
+    }
+
+
+}
+public class GetMedian66 {
+    public static void main(String[] args){
+
+    }
+}
